64af32cc965c8668be02b6ecb64ca3bc220168cb
Ian Derrington <ian.derrington@gmail.com>
2025-01-18 22:14:14 -0800

    update setup.py

--- setup.py
+++ setup.py
@@ -9,12 +9,12 @@ with open("requirements.txt", "r", encoding="utf-8") as fh:
 setup(
     name="mkdocs-slides",
     version="0.1.0",
-    author="Your Name",
-    author_email="your.email@example.com",
+    author="Ian Derrington",
+    author_email="Ian.Derrington@gmail.com",
     description="A MkDocs plugin for creating slide presentations within documentation",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/mkdocs-slides",
+    url="https://github.com/ianderrington/mkdocs-slides",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
@@ -22,6 +22,7 @@ setup(
         "mkdocs_slides": [
             "static/css/*.css",
             "static/js/*.js",
+            "templates/*.html",
         ],
     },
     install_requires=requirements,
